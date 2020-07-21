48dfa294f87cb6a8052af212fb98602bdfaa8858
Anya Brun <tsbrun@icloud.com>
2020-07-21 18:01:05 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,6 @@
 def reverse_each_word(sentence)
-  # turn string into an array
-  ## sentence_array = sentence.split(/ /)
-  # create a new array to save reversed words
-  ## reversed_array = []
-  # use .each to reverse each element of the array and save it into the reversed_array
-  ## sentence_array.each {|word| reversed_array << word.reverse}
-  # .join the array with " "
-  ## reversed_array.join(" ")
+  sentence.split(/ /).collect {|word| word.reverse}.join(" ")
 
-  # turn string into an array
-  sentence_array = sentence.split(/ /)
-  # use .collect to reverse each element of the array
-  reversed_array = sentence_array.collect {|word| word.reverse}
-  # .join the array with " "
-  reversed_array.join(" ")
+  # .each method
+  # sentence.split(/ /).each {|word| reversed_array << word.reverse}.join(" ")
 end
